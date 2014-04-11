f5d2ef4b12bb7090c7f2466355fcb2f62b4360a5
Søren Howe Gersager <syrelyre@gmail.com>
2014-04-11 18:39:53 +0200

    moved mafia-relevant to roles.py

--- roles.py
+++ roles.py
@@ -1,44 +1,137 @@
+import tornado
+
 class Game(object):
     cycle=0
     userList = {}
     roleList=[]
 
-
     def __init__(self, waiters):
         self.waiters = waiters
         self.compileRoleList()
-        self.giveRoles(waiters)
+        self.giveRoles()
+
+    def __str__(self):
+        return ("cycle: ", cycle, " - UserList: ", userList)
 
     def compileRoleList(self):
         count=0
+        length = len(self.waiters)
+        print("waiters/players: ", length)
+        mafias = length/3.5
+        benign=0
+        hostile=0
+        evil=0
+        if (length >= 11):
+            hostile = 1
+        if (length >= 7):
+            benign = 1
+        if (length >= 14):
+            evil = 1
+        towns = length-(benign+hostile+mafias)
         for user in self.waiters:
-            count+=1
+            count += 1
+
             #add another random class, some method
             # calculate how many mafia, town etc, and random between roles
-            if (count==1):
-                self.roleList.append(Doctor())
-            elif (count==2):
-                self.roleList.append(Godfather())
-            else:
-                self.roleList.append("I am some role %s <--" % count)
+            if mafias > 0:
+                self.roleList.append("Mafia Role")
+                mafias -= 1
+            elif hostile > 0:
+                self.roleList.append("Hostile Role")
+                hostile -= 1
+            elif benign > 0:
+                self.roleList.append("Benign Role")
+                benign -= 1
+            elif evil > 0:
+                self.roleList.append("Evil Role")
+                evil -= 1
+            elif towns > 0:
+                self.roleList.append("Town Role")
+                towns -= 1
+
         print(self.roleList)
 
-    def giveRoles(self, waiters):
+    def giveRoles(self):
         count=0
-        for user in waiters:
+        currentLen = len(self.waiters)
+        if len(self.waiters)!=len(self.roleList):
+            print("WTF len(waiters!=len(self.roleList)!!!? Why not the same?")
+        for user in self.waiters:
             count+=1
-            print("player: ", count)
-            self.userList[user]=self.roleList[count-1]
+            roleNo = randint(0,currentLen-count)
+            self.userList[user]=self.roleList[roleNo]
+            #self.roleList.remove(self.roleList[roleNo])
+            del self.roleList[roleNo]
         print(self.userList)
 
 class Role(object):
-    name=""
+    number=0
+    align="NoAlignYetForThisRole"
+    healed=False
+    jailed=False
+    abilityNight=False
+    abilityAvail=False
+    immune=False
+    sheriffMess = "Does not have sheriff Message yet."
+    investMess = "Does not have investigator Message yet."
+    isAlive = True
+    LW = "Insert Last Will here."
+    name="NoNameYetForThisRole" #always override
+
+    def __init__(self, user):
+       self.user=user
+
+    def setLW(self, newLW):
+        self.LW=newLW
+
+    def useAbility(self, target):
+        #override
+        pass
+
+    def getLW(self ):
+        return self.LW
 
     def __str__(self):
-     return str(self.name)
+     return self.name
 
-class Doctor(object):
+class Doctor(Role):
     name="Doctor"
 
-class Godfather(object):
+class Godfather(Role):
     name="Godfather"
+
+class Room(tornado.websocket.WebSocketHandler):
+    roomNo=0
+    players = []
+
+    def __init__(self):
+        Room.roomNo += 1
+
+    def open(self):
+        self.players.append(self)
+
+    def on_close(self):
+        self.players.remove(self)
+
+    def on_message(self, message):
+        logging.info("mess:%r", message)
+        parsed = tornado.escape.json_decode(message)
+        if parsed["body"] is None or len(parsed["body"])<1:     #avoids printing empty messages
+            return
+
+        else:
+            chat = {
+            "id": str(uuid.uuid4()),        #Hvad skal vi bruge det lort til? Får error hvis man fjerner det...wtf bliver ikke brugt xD?
+            "body": parsed["body"],
+            "nameId": (" %s says " % parsed["name"]),
+            }
+            #chat["html"] = tornado.escape.to_basestring(self.render_string("message.html", message=chat))
+            for player in self.players:
+                try:
+                    player.write_message(chat)
+                except:
+                    logging.error("Error sending message", exc_info=True)
+
+'''
+                                TORNADO STARTS HERE!!       TORNADO STARTS HERE!!       TORNADO STARTS HERE!!       TORNADO STARTS HERE!!       TORNADO STARTS HERE!!
+'''
